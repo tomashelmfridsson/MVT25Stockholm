122fae38f64d4316ca1e00da00e115c4137ba396
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-10-23 09:30:01 +0200

    improved Names

--- src/exceptions/Names.java
+++ src/exceptions/Names.java
@@ -6,22 +6,30 @@ import java.util.Scanner;
 public class Names {
 
     public static void main(String[] args) {
-        String[] names = {"Ada", "Beda", "Cålle"};
 
+        String[] names = {"Ada", "Beda", "Cålle"};
+        //0    1        2
+        int index = 5;
+        System.out.println(names[index]);
         Scanner scan = new Scanner(System.in);
+
+
         System.out.print("Skriv en siffra: ");
 
         try {
-            int index = scan.nextInt();
-            System.out.println(names[index]);
-        } catch (ArrayIndexOutOfBoundsException aiobe) {
-            System.out.println("indexet du angav fanns inte i arrayen " + aiobe);
-        } catch (InputMismatchException ime){
-            System.out.println("Input fel "+ime);
-        }catch (Exception exception){
-            System.out.println("Något knepigt hände "+exception);
+            index = scan.nextInt();
+        } catch (InputMismatchException ime) {
+            System.out.println("Input fel " + ime);
+        } catch (Exception exception) {
+            System.out.println("Något knepigt hände " + exception);
         }
-
+        try {
+            System.out.println(names[index]);
+        } catch ( ArrayIndexOutOfBoundsException aiobe) {
+            System.out.println("indexet du angav fanns inte i arrayen " + aiobe);
+        } catch (Exception exception) {
+            System.out.println("Något knepigt hände " + exception);
+        }
     }
 
 }
