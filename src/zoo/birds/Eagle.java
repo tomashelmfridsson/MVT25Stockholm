30fbf3b72a7a4beb1e083f5e93f3b20d78bbf23a
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-11-06 12:06:59 +0100

    Fibonacci skal för rekursivt och iterativt

--- src/zoo/birds/Eagle.java
+++ src/zoo/birds/Eagle.java
@@ -3,9 +3,13 @@ package zoo.birds;
 import zoo.animals.Animal;
 
 public class Eagle extends Animal {
+    public Eagle(String color) {
+        super(color);
+    }
 
     //@Overide
     // mat(){
     // return "mus"
-    //}
+
+
 }
