753c2f29b17ffe495d42a3012e71c385821a6e31
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-11-06 12:06:29 +0100

    Hashmap nameAge skal

---
+++ src/hashmap_nameage/NameAgeTest.java
@@ -0,0 +1,32 @@
+package hashmap_nameage;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class NameAgeTest {
+    @Test
+    public void testNameAge(){
+        NameAge nameAge = new NameAgeTest();
+        nameAge.addPerson("Pelle",14);
+        assertEquals(14,nameAge.getAge("Pelle"));
+    }
+    @Test
+    public void testMultipleNamesAge(){
+        NameAge nameAge = new NameAgeTest();
+        nameAge.addPerson("Pelle",14);
+        nameAge.addPerson("Olle",15);
+        nameAge.addPerson("Nilla",12);
+        assertEquals(14,nameAge.getAge("Pelle"));
+        assertEquals(15,nameAge.getAge("Olle"));
+        assertEquals(12,nameAge.getAge("Nilla"));
+    }
+    @Test
+    public void testKeyNameNotExist() {
+        NameAge nameAge = new NameAgeTest();
+        assertNull(nameAge.getAge("Pelle"));
+    }
+    @Test
+    public void testRetypeKey(){
+    }
+    @Test
+    public void testAgeMissing(){
+    }
+}
